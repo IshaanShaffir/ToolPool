5ea083bdd541a1b83017312c5a072a919a158f1d
Rowan Fortier <rowan.fortier@gmail.com>
2025-03-04 13:06:30 -0800

    add a test comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.androidpractice.toolpool"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.androidpractice.toolpool"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
